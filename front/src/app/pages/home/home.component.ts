50b60fe118f6c5d8e53299c0eb2998eb16d6d4ac
davifariasp <davi.informatica8@gmail.com>
2024-11-11 18:25:18 -0300

    feature: pagina home

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PostComponent } from "../../components/post/post.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [PostComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
